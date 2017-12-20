324acc7d53e14ea1e182b2b47969666153df1b41
Mike Dalessio <mike.dalessio@gmail.com>
2017-12-20 13:28:09 -0500

    day 7 star 2

--- day7_test.go
+++ day7_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/MakeNowJust/heredoc"
@@ -14,8 +15,64 @@ import (
 
 type ProgramNode struct {
 	name     string
+	weight   int
 	children ProgramNodes
 	parent   *ProgramNode
+}
+
+func (pn *ProgramNode) recursiveWeight() int {
+	weight := pn.weight
+	for _, child := range pn.children {
+		weight += child.recursiveWeight()
+	}
+	return weight
+}
+
+func (pn *ProgramNode) weightCheck() (*ProgramNode, int) {
+	// terminate on leaf nodes
+	if len(pn.children) == 0 {
+		return nil, -1
+	}
+
+	// depth-first search ...
+	for _, child := range pn.children {
+		problemNode, rightWeight := child.weightCheck()
+		if problemNode != nil {
+			return problemNode, rightWeight
+		}
+	}
+
+	// look at children, bucket recursive weights
+	childWeightMap := make(map[int]int) // weight → count
+	for _, child := range pn.children {
+		weight := child.recursiveWeight()
+		_, ok := childWeightMap[weight]
+		if ok {
+			childWeightMap[weight] += 1
+		} else {
+			childWeightMap[weight] = 1
+		}
+	}
+	if len(childWeightMap) == 1 {
+		return nil, -2
+	}
+
+	// if there's an outlier, go through children and find it
+	var problemWeight, okWeight int
+	for weight, count := range childWeightMap {
+		if count == 1 {
+			problemWeight = weight
+		} else {
+			okWeight = weight
+		}
+	}
+	for _, child := range pn.children {
+		if child.recursiveWeight() == problemWeight {
+			return child, child.weight + okWeight - problemWeight
+		}
+	}
+
+	return nil, -3
 }
 
 type ProgramNodes []*ProgramNode
@@ -41,9 +98,10 @@ func NewProgramTree(description string) *ProgramNode {
 		if len(line) > 0 {
 			matches := programSelfDescriptionRe.FindStringSubmatch(line)
 			name := matches[1]
+			weight, _ := strconv.Atoi(matches[2])
 			children := matches[3]
 
-			programNode := ProgramNode{name: name}
+			programNode := ProgramNode{name: name, weight: weight}
 			programMap[name] = &programNode
 
 			if len(children) > 0 {
@@ -95,10 +153,45 @@ var _ = Describe("Day7", func() {
 			cntj (57)
 		`)
 
-		It("constructs a tree of names", func() {
-			root := NewProgramTree(testData)
-			Expect(root.name).To(Equal("tknk"))
-			Expect(root.children.names()).To(ConsistOf([]string{"ugml", "padx", "fwft"}))
+		var root *ProgramNode
+
+		BeforeEach(func() {
+			root = NewProgramTree(testData)
+		})
+
+		Describe("NewProgramTree", func() {
+			It("constructs a tree of names", func() {
+				Expect(root.name).To(Equal("tknk"))
+				Expect(root.children.names()).To(ConsistOf([]string{"ugml", "padx", "fwft"}))
+			})
+
+			It("stores each program's weight", func() {
+				Expect(root.weight).To(Equal(41))
+			})
+		})
+
+		Describe("recursiveWeight", func() {
+			It("adds the weight of itself to the recursive weight of all children", func() {
+				weightMap := make(map[string]int) // child name to weight
+				for _, child := range root.children {
+					weightMap[child.name] = child.recursiveWeight()
+				}
+				Expect(weightMap["ugml"]).To(Equal(251))
+				Expect(weightMap["padx"]).To(Equal(243))
+				Expect(weightMap["fwft"]).To(Equal(243))
+			})
+		})
+
+		Describe("weightCheck", func() {
+			It("returns the node that is the wrong weight", func() {
+				wrongNode, _ := root.weightCheck()
+				Expect(wrongNode.name).To(Equal("ugml"))
+			})
+
+			It("returns the weight that the node should be", func() {
+				_, rightWeight := root.weightCheck()
+				Expect(rightWeight).To(Equal(60))
+			})
 		})
 	})
 
@@ -109,6 +202,12 @@ var _ = Describe("Day7", func() {
 		It("answers star 1 correctly", func() {
 			pt := NewProgramTree(tree_description)
 			fmt.Printf("d7 s1: tree root is %s\n", pt.name)
+		})
+
+		It("answers star 2 correctly", func() {
+			pt := NewProgramTree(tree_description)
+			wrongNode, rightWeight := pt.weightCheck()
+			fmt.Printf("d7 s2: wrong node %s, should have weight %d\n", wrongNode.name, rightWeight)
 		})
 	})
 })
