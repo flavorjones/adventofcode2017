e236da48370150faa1523b3e28b2336b97f53302
Mike Dalessio <mike.dalessio@gmail.com>
2017-12-26 14:22:38 -0500

    wip: changed to list impl

--- day17_test.go
+++ day17_test.go
@@ -1,33 +1,41 @@
 package adventofcode2017_test
 
 import (
+	"container/list"
 	"fmt"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-type SpinLockBuffer []int
-
 type SpinLock struct {
 	stepSize int
-	buffer   SpinLockBuffer
-	position int
+	buffer   *list.List
+	cursor   *list.Element
 	count    int
 }
 
 func NewSpinLock(stepSize int) *SpinLock {
-	return &SpinLock{position: 0, stepSize: stepSize, buffer: []int{0}}
+	list := list.New()
+	list.PushFront(0)
+	cursor := list.Front()
+	return &SpinLock{stepSize: stepSize, buffer: list, cursor: cursor}
+}
+
+func (s *SpinLock) advanceCursor() {
+	s.cursor = s.cursor.Next()
+	if s.cursor == nil {
+		s.cursor = s.buffer.Front()
+	}
 }
 
 func (s *SpinLock) insert() {
 	s.count++
-	s.position = (s.position+s.stepSize)%len(s.buffer) + 1
-
-	// insert without allocating a whole new slice
-	s.buffer = append(s.buffer, 0)
-	copy(s.buffer[s.position+1:], s.buffer[s.position:])
-	s.buffer[s.position] = s.count
+	for j := 0; j < s.stepSize; j++ {
+		s.advanceCursor()
+	}
+	s.buffer.InsertAfter(s.count, s.cursor)
+	s.advanceCursor()
 }
 
 func (s *SpinLock) insertN(n int) {
@@ -39,13 +47,22 @@ func (s *SpinLock) insertN(n int) {
 	}
 }
 
-func (s *SpinLock) indexOf(desired int) int {
-	for j, n := range s.buffer {
-		if n == desired {
-			return j
+func (s *SpinLock) cursorOf(desired int) *list.Element {
+	for e := s.buffer.Front(); e != nil; e = e.Next() {
+		if e.Value == desired {
+			return e
 		}
 	}
-	return -1
+	return nil
+}
+
+// primarily for testing
+func (s *SpinLock) toSlice() []int {
+	rval := make([]int, s.buffer.Len())
+	for j, e := 0, s.buffer.Front(); e != nil; j, e = j+1, e.Next() {
+		rval[j] = e.Value.(int)
+	}
+	return rval
 }
 
 var _ = Describe("Day17", func() {
@@ -58,12 +75,12 @@ var _ = Describe("Day17", func() {
 
 			It("should have an initial circular buffer state", func() {
 				s := NewSpinLock(12)
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0}))
+				Expect(s.toSlice()).To(Equal([]int{0}))
 			})
 
-			It("should have an initial pointer position", func() {
+			It("should have an initial cursor", func() {
 				s := NewSpinLock(12)
-				Expect(s.position).To(Equal(0))
+				Expect(s.cursor.Value).To(Equal(0))
 			})
 		})
 
@@ -72,57 +89,56 @@ var _ = Describe("Day17", func() {
 				s := NewSpinLock(3)
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 1}))
-				Expect(s.position).To(Equal(1))
+				Expect(s.toSlice()).To(Equal([]int{0, 1}))
+				Expect(s.cursor.Value).To(Equal(1))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 2, 1}))
-				Expect(s.position).To(Equal(1))
+				Expect(s.toSlice()).To(Equal([]int{0, 2, 1}))
+				Expect(s.cursor.Value).To(Equal(2))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 2, 3, 1}))
-				Expect(s.position).To(Equal(2))
+				Expect(s.toSlice()).To(Equal([]int{0, 2, 3, 1}))
+				Expect(s.cursor.Value).To(Equal(3))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 2, 4, 3, 1}))
-				Expect(s.position).To(Equal(2))
+				Expect(s.toSlice()).To(Equal([]int{0, 2, 4, 3, 1}))
+				Expect(s.cursor.Value).To(Equal(4))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 5, 2, 4, 3, 1}))
-				Expect(s.position).To(Equal(1))
+				Expect(s.toSlice()).To(Equal([]int{0, 5, 2, 4, 3, 1}))
+				Expect(s.cursor.Value).To(Equal(5))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 5, 2, 4, 3, 6, 1}))
-				Expect(s.position).To(Equal(5))
+				Expect(s.toSlice()).To(Equal([]int{0, 5, 2, 4, 3, 6, 1}))
+				Expect(s.cursor.Value).To(Equal(6))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 5, 7, 2, 4, 3, 6, 1}))
-				Expect(s.position).To(Equal(2))
+				Expect(s.toSlice()).To(Equal([]int{0, 5, 7, 2, 4, 3, 6, 1}))
+				Expect(s.cursor.Value).To(Equal(7))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 5, 7, 2, 4, 3, 8, 6, 1}))
-				Expect(s.position).To(Equal(6))
+				Expect(s.toSlice()).To(Equal([]int{0, 5, 7, 2, 4, 3, 8, 6, 1}))
+				Expect(s.cursor.Value).To(Equal(8))
 
 				s.insert()
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 9, 5, 7, 2, 4, 3, 8, 6, 1}))
-				Expect(s.position).To(Equal(1))
+				Expect(s.toSlice()).To(Equal([]int{0, 9, 5, 7, 2, 4, 3, 8, 6, 1}))
+				Expect(s.cursor.Value).To(Equal(9))
 
 				for j := 10; j <= 2017; j++ {
 					s.insert()
 				}
-				Expect(s.buffer[s.position]).To(Equal(2017))
-				Expect(s.buffer[s.position+1]).To(Equal(638))
+				Expect(s.cursor.Value).To(Equal(2017))
+				Expect(s.cursor.Next().Value).To(Equal(638))
 			})
 		})
 
-		Describe("indexOf()", func() {
+		Describe("cursorOf()", func() {
 			It("returns the index of the number in the buffer", func() {
 				s := NewSpinLock(3)
 				s.insertN(9)
-				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 9, 5, 7, 2, 4, 3, 8, 6, 1}))
-				Expect(s.indexOf(4)).To(Equal(5))
-				Expect(s.indexOf(0)).To(Equal(0))
-				Expect(s.indexOf(1)).To(Equal(9))
+				Expect(s.cursorOf(4).Value).To(Equal(4))
+				Expect(s.cursorOf(0).Value).To(Equal(0))
+				Expect(s.cursorOf(1).Value).To(Equal(1))
 			})
 		})
 	})
@@ -131,8 +147,8 @@ var _ = Describe("Day17", func() {
 		It("solves star 1", func() {
 			s := NewSpinLock(312)
 			s.insertN(2017)
-			Expect(s.buffer[s.position]).To(Equal(2017))
-			answer := s.buffer[s.position+1]
+			Expect(s.cursor.Value).To(Equal(2017))
+			answer := s.cursor.Next().Value
 			fmt.Printf("d17 s1: short-circuit spinlock with %d\n", answer)
 		})
 
