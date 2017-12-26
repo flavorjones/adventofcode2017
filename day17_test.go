4833e08d4c83fc5953e12caea0b66d2e4279f63b
Mike Dalessio <mike.dalessio@gmail.com>
2017-12-26 13:53:57 -0500

    preparing for star 2, but too slow
    likely need a linked list

--- day17_test.go
+++ day17_test.go
@@ -30,10 +30,22 @@ func (s *SpinLock) insert() {
 	s.buffer[s.position] = s.count
 }
 
-func (s *SpinLock) insert2017() {
-	for j := 1; j <= 2017; j++ {
+func (s *SpinLock) insertN(n int) {
+	for j := 1; j <= n; j++ {
+		if (j % 100000) == 0 {
+			fmt.Printf("insert %d", j)
+		}
 		s.insert()
 	}
+}
+
+func (s *SpinLock) indexOf(desired int) int {
+	for j, n := range s.buffer {
+		if n == desired {
+			return j
+		}
+	}
+	return -1
 }
 
 var _ = Describe("Day17", func() {
@@ -102,15 +114,34 @@ var _ = Describe("Day17", func() {
 				Expect(s.buffer[s.position+1]).To(Equal(638))
 			})
 		})
+
+		Describe("indexOf()", func() {
+			It("returns the index of the number in the buffer", func() {
+				s := NewSpinLock(3)
+				s.insertN(9)
+				Expect(s.buffer).To(Equal(SpinLockBuffer{0, 9, 5, 7, 2, 4, 3, 8, 6, 1}))
+				Expect(s.indexOf(4)).To(Equal(5))
+				Expect(s.indexOf(0)).To(Equal(0))
+				Expect(s.indexOf(1)).To(Equal(9))
+			})
+		})
 	})
 
 	Describe("puzzle", func() {
 		It("solves star 1", func() {
 			s := NewSpinLock(312)
-			s.insert2017()
+			s.insertN(2017)
 			Expect(s.buffer[s.position]).To(Equal(2017))
 			answer := s.buffer[s.position+1]
 			fmt.Printf("d17 s1: short-circuit spinlock with %d\n", answer)
 		})
+
+		// It("solves star 2", func() {
+		// 	s := NewSpinLock(312)
+		// 	s.insertN(50000000)
+		// 	index := s.indexOf(0)
+		// 	answer := s.buffer[index+1]
+		// 	fmt.Printf("d17 s2: short-circuit spinlock with %d\n", answer)
+		// })
 	})
 })
