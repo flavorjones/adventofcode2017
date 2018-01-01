b3c2608b803adf85487531774626a78994d276aa
Mike Dalessio <mike.dalessio@gmail.com>
2018-01-01 16:29:12 -0500

    prefactor for star 2

--- day22_test.go
+++ day22_test.go
@@ -29,15 +29,24 @@ var virusTurnRight = map[CartesianCoordinates]CartesianCoordinates{
 	virusLeft:  virusUp,
 }
 
+type InfectionStatus int
+
+const (
+	InfectionStatusClean    = InfectionStatus(0)
+	InfectionStatusWeakened = InfectionStatus(1)
+	InfectionStatusInfected = InfectionStatus(2)
+	InfectionStatusFlagged  = InfectionStatus(3)
+)
+
 type SporificaVirus struct {
-	infected   map[CartesianCoordinates]bool
+	infected   map[CartesianCoordinates]InfectionStatus
 	position   CartesianCoordinates
 	direction  CartesianCoordinates
 	infections int
 }
 
 func NewSporificaVirus(nodeMap string) *SporificaVirus {
-	sv := SporificaVirus{direction: virusUp, infected: make(map[CartesianCoordinates]bool)}
+	sv := SporificaVirus{direction: virusUp, infected: make(map[CartesianCoordinates]InfectionStatus)}
 
 	nodeMapLines := strings.Split(nodeMap, "\n")
 	size := len(nodeMapLines[0])
@@ -46,7 +55,7 @@ func NewSporificaVirus(nodeMap string) *SporificaVirus {
 		for jcol, char := range []byte(line) {
 			if char == '#' {
 				coords := CartesianCoordinates{jcol - offset, offset - jrow}
-				sv.infected[coords] = true
+				sv.infected[coords] = InfectionStatusInfected
 			}
 		}
 	}
@@ -54,21 +63,21 @@ func NewSporificaVirus(nodeMap string) *SporificaVirus {
 	return &sv
 }
 
-func (sv *SporificaVirus) NodeInfected(coords CartesianCoordinates) bool {
+func (sv *SporificaVirus) NodeInfected(coords CartesianCoordinates) InfectionStatus {
 	result, ok := sv.infected[coords]
 	if !ok {
-		return false
+		return InfectionStatusClean
 	}
 	return result
 }
 
 func (sv *SporificaVirus) Burst() {
-	if sv.NodeInfected(sv.position) {
-		sv.infected[sv.position] = false
+	if sv.NodeInfected(sv.position) == InfectionStatusInfected {
+		sv.infected[sv.position] = InfectionStatusClean
 		sv.direction = virusTurnRight[sv.direction]
 	} else {
 		sv.infections++
-		sv.infected[sv.position] = true
+		sv.infected[sv.position] = InfectionStatusInfected
 		sv.direction = virusTurnLeft[sv.direction]
 	}
 	sv.position = sv.position.move(sv.direction)
@@ -95,9 +104,9 @@ var _ = Describe("Day22", func() {
 
 			It("stores infected node positions", func() {
 				sv := NewSporificaVirus(testMap)
-				Expect(sv.NodeInfected(CartesianCoordinates{0, 0})).To(BeFalse())
-				Expect(sv.NodeInfected(CartesianCoordinates{1, 1})).To(BeTrue())
-				Expect(sv.NodeInfected(CartesianCoordinates{-1, 0})).To(BeTrue())
+				Expect(sv.NodeInfected(CartesianCoordinates{0, 0})).To(Equal(InfectionStatusClean))
+				Expect(sv.NodeInfected(CartesianCoordinates{1, 1})).To(Equal(InfectionStatusInfected))
+				Expect(sv.NodeInfected(CartesianCoordinates{-1, 0})).To(Equal(InfectionStatusInfected))
 			})
 		})
 
@@ -107,13 +116,13 @@ var _ = Describe("Day22", func() {
 
 				BeforeEach(func() {
 					sv = NewSporificaVirus(testMap)
-					Expect(sv.NodeInfected(sv.position)).To(BeFalse())
+					Expect(sv.NodeInfected(sv.position)).To(Equal(InfectionStatusClean))
 				})
 
 				It("infects the node, turns left, moves forward", func() {
 					position := sv.position
 					sv.Burst()
-					Expect(sv.NodeInfected(position)).To(BeTrue())
+					Expect(sv.NodeInfected(position)).To(Equal(InfectionStatusInfected))
 					Expect(sv.direction).To(Equal(virusLeft))
 					Expect(sv.position).To(Equal(CartesianCoordinates{-1, 0}))
 					Expect(sv.infections).To(Equal(1))
@@ -126,14 +135,14 @@ var _ = Describe("Day22", func() {
 				BeforeEach(func() {
 					sv = NewSporificaVirus(testMap)
 					sv.Burst()
-					Expect(sv.NodeInfected(sv.position)).To(BeTrue())
+					Expect(sv.NodeInfected(sv.position)).To(Equal(InfectionStatusInfected))
 					Expect(sv.direction).To(Equal(virusLeft))
 				})
 
 				It("cleans the node, turns right, moves forward", func() {
 					position := sv.position
 					sv.Burst()
-					Expect(sv.NodeInfected(position)).To(BeFalse())
+					Expect(sv.NodeInfected(position)).To(Equal(InfectionStatusClean))
 					Expect(sv.direction).To(Equal(virusUp))
 					Expect(sv.position).To(Equal(CartesianCoordinates{-1, 1}))
 					Expect(sv.infections).To(Equal(1))
@@ -164,7 +173,7 @@ var _ = Describe("Day22", func() {
 
 		It("solves star 1", func() {
 			sv := NewSporificaVirus(nodeMap)
-			Expect(sv.NodeInfected(CartesianCoordinates{-12, 12})).To(BeTrue())
+			Expect(sv.NodeInfected(CartesianCoordinates{-12, 12})).To(Equal(InfectionStatusInfected))
 
 			for j := 1; j <= 10000; j++ {
 				sv.Burst()
